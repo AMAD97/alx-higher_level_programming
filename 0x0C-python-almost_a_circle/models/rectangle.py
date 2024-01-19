2b2b73cae6372892ac5c105ce2cd23ef2a6f7e02
AMAD97 <aminedaoui00@gmail.com>
2024-01-19 02:19:36 -0800

    first rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Module that defines a Rectangle class """
-
+'''Module that defines a Rectangle class.'''
 from models.base import Base
 
+
 class Rectangle(Base):
-    """ Rectangle class inherits from Base """
+    '''Rectangle class inherits from Base.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Initializes a Rectangle instance """
+        '''Initializes a Rectangle instance.'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,40 +16,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ Getter for width """
+        '''Getter for width.'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Setter for width """
+        '''Setter for width.'''
         self.__width = value
 
     @property
     def height(self):
-        """ Getter for height """
+        '''.Getter for height.'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Setter for height """
+        '''Setter for height.'''
         self.__height = value
 
     @property
     def x(self):
-        """ Getter for x """
+        '''Getter for x.'''
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ Setter for x """
+        '''Setter for x.'''
         self.__x = value
 
     @property
     def y(self):
-        """ Getter for y """
+        '''Getter for y.'''
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ Setter for y """
+        '''Setter for y.'''
         self.__y = value
